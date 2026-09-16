a97e3e97e9335af6d5f6e94dfbe133292294f638
abhinav <abhinavpadige06@gmail.com>
2026-09-16 10:09:30 +0530

    Create initial home page

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <div className="flex-1">
+        <h1 className="text-center text-4xl font-bold py-12">
+          Amazon SDE Portfolio
+        </h1>
+      </div>
+    </main>
+  );
+}
