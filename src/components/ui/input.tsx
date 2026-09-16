45c77cf26cc7acf6c0017f3ee00ee25f2a2dd43e
abhinav <abhinavpadige06@gmail.com>
2026-09-16 10:09:51 +0530

    Create Input component

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const inputVariants = cva(
+  "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "",
+        destructive:
+          "border-destructive text-destructive-foreground placeholder-destructive-foreground",
+        outline: "border-input hover:bg-accent",
+        secondary:
+          "border-secondary text-secondary-foreground placeholder-secondary-foreground",
+      },
+      size: {
+        default: "h-10 py-2 px-3",
+        sm: "h-9 px-2",
+        lg: "h-11 px-4",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, variant, size, ...props }, ref) => (
+    <input
+      className={cn(inputVariants({ variant, size, className }))}
+      ref={ref}
+      {...props}
+    />
+  )
+);
+Input.displayName = "Input";
+
+export { Input, inputVariants };
